188a00f7f59604f50fff4866d4ff3b4203a1677f
dcasmar <32893220+dcasmar@users.noreply.github.com>
2017-11-12 18:52:38 +0100

    Add files via upload

---
+++ Server-Escritorio-Remoto-master/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    Private clients As Integer = -1
+    Dim WithEvents sv As New server
+    Public WithEvents tmrConect As New Timer
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim port As Integer = Integer.Parse(txtPort.Text)
+        startListen(port)
+    End Sub
+    Public Sub startListen(ByVal port As Integer)
+        sv.PuertoDeEscucha = port
+        sv.start()
+        tmrConect.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        sv.sendDisplay()
+    End Sub
+
+    Private Sub btnSend_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSend.Click
+        Timer1.Start()
+    End Sub
+    Public Sub newClientFrm(ByVal countCLients As Integer, ByVal idClient As String)
+        sv.flagNewClient = "None"
+        showClients()
+        Label1.Text = "Usuarios conectados [" & sv.clients.Count & "]"
+    End Sub
+    Public Sub checkModifyClient() Handles tmrConect.Tick
+        If sv.flagNewClient <> "None" Then
+            newClientFrm(sv.clients.Count, sv.flagNewClient)
+        End If
+        If sv.flagRemoveClient <> "None" Then
+            removeClientFrm(sv.flagRemoveClient)
+        End If
+    End Sub
+    Public Sub removeClientFrm(ByVal idClient As String)
+        Try
+            'sv.flagRemoveClient = "None"
+            'pnUsersConect.Controls.Remove(pnUsersConect.Controls.Item(idClient))
+            sv.flagRemoveClient = "None"
+            showClients()
+            Label1.Text = "Usuarios conectados [" & sv.clients.Count & "]"
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+
+    End Sub
+    Public Sub acceptClient(ByVal sender As Object, ByVal e As System.EventArgs)
+        Dim strEndPoint As String
+        strEndPoint = sender.name
+        Debug.Print("Llego: " & sender.name.ToString)
+        If sv.clientAutorized = strEndPoint Then
+            sv.clientAutorized = ""
+            sender.text = "Pemitir Control"
+        Else
+            sender.text = "Detener Control"
+            sv.clientAutorized = strEndPoint
+        End If
+
+    End Sub
+
+    Public Sub showClients()
+
+        pnUsersConect.Controls.Clear()
+        Dim i As Integer = 0
+        If sv.clients.Count > 0 Then
+            For Each Cliente In sv.clients.Values
+                Dim name As String = Cliente.id.ToString
+                Dim itemNumbers As Integer = i
+                Dim itemeUser As New ItemUser
+                itemeUser.LnkAllowControl.Name = name
+                itemeUser.lblName.Text = Cliente.id.ToString()
+                itemeUser.Location = New Point(5, (itemNumbers * 65) + 1)
+                itemeUser.Size = New Size(pnUsersConect.Width - 11, 61)
+                pnUsersConect.Controls.Add(itemeUser)
+                AddHandler itemeUser.LnkAllowControl.Click, AddressOf acceptClient
+                i = i + 1
+            Next
+
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        End
+    End Sub
+End Class
---
+++ Server-Escritorio-Remoto-master/ItemUser.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ItemUser
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.LnkAllowControl = New System.Windows.Forms.LinkLabel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.viewerServer.My.Resources.Resources.noimagen
+        Me.PictureBox1.Location = New System.Drawing.Point(11, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(41, 41)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(65, 13)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(43, 13)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Usuario"
+        '
+        'LnkAllowControl
+        '
+        Me.LnkAllowControl.AutoSize = True
+        Me.LnkAllowControl.Location = New System.Drawing.Point(65, 33)
+        Me.LnkAllowControl.Name = "LnkAllowControl"
+        Me.LnkAllowControl.Size = New System.Drawing.Size(77, 13)
+        Me.LnkAllowControl.TabIndex = 2
+        Me.LnkAllowControl.TabStop = True
+        Me.LnkAllowControl.Text = "Permitir Control"
+        '
+        'ItemUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Controls.Add(Me.LnkAllowControl)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "ItemUser"
+        Me.Size = New System.Drawing.Size(319, 61)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents LnkAllowControl As System.Windows.Forms.LinkLabel
+
+End Class
